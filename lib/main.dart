110691106739d2b95b5033f480ca434c68b105b0
Ashwini Kumar <37498182+Ash993@users.noreply.github.com>
2021-04-09 16:02:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -19,9 +21,11 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  SearchBar searchBar;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   var url =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-
+    // final ScrollController _scrollController = ScrollController();
   PokeHub pokeHub;
 
   @override
@@ -38,63 +42,94 @@ class HomePageState extends State<HomePage> {
     print(pokeHub.toJson());
     setState(() {});
   }
+  AppBar buildAppBar(BuildContext context) {
+    return new AppBar(
+        title: Text("Poke App"),
+        backgroundColor: Colors.cyan,
+        actions: [searchBar.getSearchAction(context)]
+    );
+  }
+  void onSubmitted(String value) {
+    setState(() => _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text('You wrote $value!'))));
+  }
+
+  HomePageState() {
+    searchBar = new SearchBar(
+        inBar: false,
+        setState: setState,
+        onSubmitted: onSubmitted,
+        buildDefaultAppBar: buildAppBar
+
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text("Poke App"),
-        backgroundColor: Colors.cyan,
-      ),
-      body: pokeHub == null
-          ? Center(
-        child: CircularProgressIndicator(),
-      )
-          : GridView.count(
-        crossAxisCount: 2,
-        children: pokeHub.pokemon
-            .map((poke) => Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => PokeDetail(
-                        pokemon: poke,
-                      )));
-            },
-            child: Hero(
-              tag: poke.img,
-              child: Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment:
-                  MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      height: MediaQuery.of(context).size.height*0.2,
-                      width: MediaQuery.of(context).size.width*0.3,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(poke.img))),
-                    ),
-                    Text(
-                      poke.name,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
+      appBar:searchBar.build(context),
+      key: _scaffoldKey,
+      
+      body: new Scrollbar(
+          isAlwaysShown: true,
+        // controller: _scrollController,
+        //
+        // radius: Radius.circular(20),
+        thickness: 20,
+
+
+
+        child: pokeHub == null
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : GridView.count(
+          crossAxisCount: 2,
+          children: pokeHub.pokemon
+              .map((poke) => Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => PokeDetail(
+                          pokemon: poke,
+                        )));
+              },
+
+              child: Hero(
+                tag: poke.img,
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment:
+                    MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        height: MediaQuery.of(context).size.height*0.2,
+                        width: MediaQuery.of(context).size.width*0.3,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(poke.img))),
                       ),
-                    )
-                  ],
+                      Text(
+                        poke.name,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ))
-            .toList(),
+          ))
+              .toList(),
+        ),
       ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
